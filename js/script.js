bac1f776c56dba84768cfddda0a04582d28ddcd1
Adil Zhexenov <nescrypnoadil@gmail.com>
2022-09-12 23:19:18 +0600

    add about us and new mobile menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,34 @@
 const mobileMenuBtn = document.querySelector('.fa-bars');
 const mobileMenuClose = document.querySelector('.fa-times');
 const mobileMenuBg = document.querySelector('.mobile__menu');
+
+function openMenu(){
+     mobileMenuBg.style.cssText = 'opacity: 1; z-index: 1000';
+}
+
+function closeMenu(){
+     mobileMenuBg.style.cssText = 'opacity: 0; z-index: -1';
+}
+
 mobileMenuBtn.addEventListener('click', function(){
-     mobileMenuBg.style.cssText = 'opacity: 1; z-index: 4';
-     document.body.style.cssText = 'overflow-y: hidden';
+     openMenu();
 });
 mobileMenuClose.addEventListener('click', function(){
-     mobileMenuBg.style.cssText = 'opacity: 0; z-index: 0';
-     document.body.style.cssText = 'overflow-y: none';
+     closeMenu();
+});
 
+mobileMenuBg.addEventListener('click', function(e){
+     if(e.target && e.target.tagName === 'A'){
+          closeMenu();
+     }
+     console.log(e);
+});
+
+document.addEventListener('scroll', function(e){
+     e.preventDefault();
+     if(this.documentElement.scrollTop > 50){
+          document.querySelector('nav').classList.add('nav__border');
+     } else{
+          document.querySelector('nav').classList.remove('nav__border');
+     }
 });
