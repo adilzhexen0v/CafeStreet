11b6b96cf234719b529297c5142e822bb0938fcb
Adil Zhexenov <nescrypnoadil@gmail.com>
2022-09-17 00:09:36 +0600

    add comments section

--- js/script.js
+++ js/script.js
@@ -212,3 +212,50 @@ if (document.documentElement.offsetWidth <= 768) {
           }
      });
 }
+
+/* Change submit button in Mobile Version */
+
+if (document.documentElement.offsetWidth <= 480){
+     const submitForm = document.querySelector('#subscribe input[type="submit"]');
+     submitForm.value = '>';
+}
+
+/* Email */
+const form = document.querySelector('#subscribe form');
+const input = document.querySelector('#subscribe input[type="text"]');
+form.addEventListener('submit', function(e){ 
+     e.preventDefault();
+     if(input.value.length > 0 && input.value.indexOf('@') > -1){
+          document.querySelector('.email').textContent = input.value;
+          showPopup();
+     } else if(input.value.length === 0){
+          alert('Fill in the email field!');
+     } else{
+          alert('Invalid value!');
+     } 
+});
+
+/* PopUp */
+const popup = document.querySelector('.popup');
+const popupContainer = document.querySelector('.popup__container');
+const popupClose = document.querySelector('.popup__close');
+
+function showPopup(){
+     popup.style.display = 'flex';
+     setTimeout(() => {
+         popupContainer.style.cssText = 'margin-top: 0'; 
+     }, 1);
+}
+
+function hidePopup(){
+     popupContainer.style.cssText = 'margin-top: -100vw'; 
+     setTimeout(() => {
+          popup.style.display = 'none';
+     }, 350);
+}
+
+popup.addEventListener('click', function(e){
+     if(e.target === popupClose || e.target.classList.contains('popup')) {
+          hidePopup();
+     }
+});
